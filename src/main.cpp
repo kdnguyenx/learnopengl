4ed22cfe9879ed3e811c5d9ed728acdfd031298b
Khoa Nguyen <khoa.nguyen@netapp.com>
2025-04-21 16:57:08 +0700

    basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main() {
+    // set context
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // create window object
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Learn OpenGL", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // init glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress(nullptr))) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
     return 0;
 }
